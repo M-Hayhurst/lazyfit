50ff0605bdce76d3541a8dfb4504794d39745f2e
Martin Hayhurst Appel <martin.ledzep@gmail.com>
2022-06-12 13:31:17 +0100

    Updated error print in utility

--- utility.py
+++ utility.py
@@ -3,30 +3,35 @@ import numpy as np
 import math as m
 
 
-def format_error(y, dy):
+def format_error(y, dy, version=1):
 	"""return a nice string representation of a number and its error"""
 	if np.isinf(dy):
-		return '%g.3(inf)' % y
+		return '%g.3±inf' % y
 	elif np.isnan(dy):
-		return '%g.3(nan)' % y
+		return '%g.3±nan' % y
+	elif dy == 0:
+		return '%g.3' % y
 
-	dy = np.abs(dy)
+	dy = np.abs(dy) # ensure  error is positive
 
 	y_exponent = int(m.floor(np.log10(np.abs(y))))  # order of magnitude
 	dy_exponent = int(m.floor(np.log10(dy)))
 
-	# choose how many digits we want on the y
-	exp_diff = y_exponent - dy_exponent  # order of magnitude diff from y to dy
-	dy_str = ('%.20g' % (dy * 10 ** (exp_diff + 2))).replace('.', '')  # string representation without any decimal
-	flag1 = str(dy)[0] == '1'  # first digit is 1
-	y_precision = exp_diff + int(flag1)  # number of digits on y after decimal
+	if version == 1:
+		'''format as (y ± dy) exponent'''
 
-	# TODO: Make error round corrently
+		# get digits on y error
+		dy_digits = 1 + int( ('%.2g'%(dy/10**dy_exponent))[0] == '1') # use two digits on dy if first digit is a 1
 
-	s = ('%.*f' % (y_precision, y / 10 ** y_exponent) + '(' +
-		 dy_str[0:(1 + int(flag1))] +
-		 ')e' + str(y_exponent))
-	return s
+		exp_diff = y_exponent - dy_exponent  # order of magnitude diff from y to dy
+
+		y_digits = exp_diff + dy_digits - 1 # number of digits on y after decimal. Add one to match the 2 error digits
+
+		s = ('(%.*f' % (y_digits, y / 10 ** y_exponent)) + \
+			'±' +\
+			'%.*f)' % (y_digits, dy / 10 ** y_exponent) + \
+			'e'+str(y_exponent)
+		return s
 
 
 def clean_data(x, y):
