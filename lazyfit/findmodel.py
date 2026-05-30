4ea38d1af48b1ae66441628c820d8c0008084ae8
Martin Hayhurst Appel <martin.ledzep@gmail.com>
2026-05-30 15:26:06 +0200

    Correctly escape backslash in regular expression in model finder.

--- lazyfit/findmodel.py
+++ lazyfit/findmodel.py
@@ -10,7 +10,7 @@ def find_model(model_name):
     '''
 
     # remove special characters and convert to lower case
-    model_name = re.sub('[\(\)\{\}<>\-]', '', model_name)
+    model_name = re.sub(r'[\(\)\{\}<>\-]', '', model_name)
     model_name = model_name.lower()
 
     # see if it exists
@@ -38,8 +38,8 @@ abbreviations = { # look up table, to convert an abbreviated model name to the m
     'quad': 'quadratic',
     'logisticpulse':'logpulse',
     'dualgauss': 'dualgaussian',
-    'quad':'quadratic',
     'poly2': 'quadratic',
-    't1':'T1' # here we need to conver back to upper case
+    't1':'T1', # here we need to conver back to upper case
+    'doublelorentz': 'duallorentz'
 }
 
